af5622d946a23810241be0a5a60d2c7968f623d1
formalfuss0112 <manojkchandrashekar1@gmail.com>
2025-08-08 18:01:57 +0530

    Simplify the boilerplate app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+function App() {
+  return (
+    <div className="App">
+      Little Lemon Restaurant 🍋
+    </div>
+  );
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+export default App;
